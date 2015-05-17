c8aee40487b1484ec6317653df926ebebe670ef4
Avner Peled <avnerus@gmail.com>
2015-05-17 16:48:47 +0300

    initial test

---
+++ demo/index.js
@@ -0,0 +1,2 @@
+require('riot');
+require('../riot-froala');
---
+++ riot-froala.js
@@ -0,0 +1,8 @@
+riot.tag('riot-froala',' \
+         <span>{message}</span> \
+ ',
+    function(opts) {
+        this.message = "Hello world!";
+    }
+);
+
