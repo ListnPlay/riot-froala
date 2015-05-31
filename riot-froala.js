c252b21bdc5e712fdc18776f055e067b98c14f9f
Avner Peled <avnerus@gmail.com>
2015-05-31 21:13:45 +0300

    fix froala editor to work with browserify

--- riot-froala.js
+++ riot-froala.js
@@ -1,8 +1,14 @@
+require('./lib/froala_editor.min');
+
 riot.tag('riot-froala',' \
-         <span>{message}</span> \
+     <div id="riot-froala-edit"></div> \
+     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/froala-editor/1.2.7/css/froala_editor.min.css"> \
+     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css"> \
  ',
     function(opts) {
-        this.message = "Hello world!! (RIOT-FROALA)";
+        this.on('mount', function() {
+            $('#riot-froala-edit').editable();
+        });
     }
 );
 
